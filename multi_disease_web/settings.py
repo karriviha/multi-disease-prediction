cd287b0d897c613ef29ca0c3dd13708fcc6422e3
Viha Karri <karriviha@gmail.com>
2026-02-26 18:22:07 +0530

    Switched to PostgreSQL database

--- multi_disease_web/settings.py
+++ multi_disease_web/settings.py
@@ -1,3 +1,5 @@
+import dj_database_url
+import os
 from pathlib import Path
 import os
 
@@ -71,10 +73,11 @@ WSGI_APPLICATION = 'multi_disease_web.wsgi.application'
 # ========================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
