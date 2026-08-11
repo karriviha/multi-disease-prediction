7c5bfaeb72583dfaa741dbb6529d8a12a42e90bc
Viha Karri <karriviha@gmail.com>
2026-08-11 17:07:08 +0530

    Switch database from PostgreSQL to SQLite

--- multi_disease_web/settings.py
+++ multi_disease_web/settings.py
@@ -1,4 +1,3 @@
-import dj_database_url
 import os
 from pathlib import Path
 import os
@@ -73,11 +72,10 @@ WSGI_APPLICATION = 'multi_disease_web.wsgi.application'
 # ========================
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
